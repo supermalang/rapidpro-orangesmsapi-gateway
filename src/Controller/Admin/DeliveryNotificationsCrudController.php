624d15489723f2d5f3eb5ca4aa17788ce16c1210
Haj Malang <supermalang@outlook.com>
2022-02-09 11:30:02 +0000

    [UPDATE] Removed action buttons in the DeliveryNotifications CRUD

--- src/Controller/Admin/DeliveryNotificationsCrudController.php
+++ src/Controller/Admin/DeliveryNotificationsCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\DeliveryNotifications;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class DeliveryNotificationsCrudController extends AbstractCrudController
@@ -22,4 +25,24 @@ class DeliveryNotificationsCrudController extends AbstractCrudController
         ];
     }
     */
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle('index', 'Delivery Statuses of sent messages')
+            ->setPageTitle('detail', 'Delivery status of message')
+        ;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->remove(Crud::PAGE_INDEX, Action::DELETE)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_DETAIL, Action::DELETE)
+            ->remove(Crud::PAGE_DETAIL, Action::EDIT)
+        ;
+    }
 }
